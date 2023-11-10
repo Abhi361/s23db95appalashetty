b30710b9194ad9d58a0d58b059d407f9cada1583
Abhishek <appalashettyabhishek23@gmail.com>
2023-11-10 01:28:21 -0600

    made changes

--- controllers/eagleController.js
+++ controllers/eagleController.js
@@ -57,3 +57,57 @@ exports.eagle_delete = function (req, res) {
 exports.eagle_update_put = function (req, res) {
     res.send('NOT IMPLEMENTED: eagle update PUT' + req.params.id);
 };
+
+// Handle Eagle detail on GET.
+exports.eagle_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        const Eagle = await eagle.findById(req.params.id);
+        if (Eagle == null) {
+            res.status(404);
+            res.send({"error": "Eagle not found"});
+        } else {
+            res.send(Eagle);
+        }
+    } catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+// Handle eagle update form on PUT.
+exports.eagle_update_put = async function(req, res) {
+    console.log(`Update on id ${req.params.id} with body ${JSON.stringify(req.body)}`);
+    try {
+        let toUpdate = await eagle.findById(req.params.id);
+
+        // Check if the document exists
+        if (!toUpdate) {
+            res.status(404).send(`{"error": "Eagle with ID ${req.params.id} not found"}`);
+            return;
+        }
+
+        // Do updates of properties
+        if (req.body.name) toUpdate.name = req.body.name;
+        if (req.body.age) toUpdate.age = req.body.age;
+        if (req.body.description) toUpdate.description = req.body.description;
+
+        // Handle checkbox (assuming it's named checkboxsale in the body)
+        if (req.body.checkboxsale) {
+            toUpdate.sale = true;
+        } else {
+            toUpdate.sale = false;
+        }
+
+        let result = await toUpdate.save();
+
+        // Include the sale property in the response
+        result = result.toObject(); // Convert Mongoose document to a plain JavaScript object
+        result.sale = toUpdate.sale;
+
+        console.log("Success " + result);
+        res.send(result);
+    } catch (err) {
+        res.status(500).send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
+};
--- routes/eagles.js
+++ routes/eagles.js
@@ -3,4 +3,7 @@ const eagle_controlers = require("../controllers/eagleController");
 var router = express.Router();
 // GET eagles
 router.get("/", eagle_controlers.eagle_view_all_Page);
-module.exports = router;
+module.exports = router;
+
+// GET request for one eagle.
+router.get('/eagles/:id', eagle_controlers.eagle_detail);
