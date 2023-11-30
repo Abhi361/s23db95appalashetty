00bc5b57d0c2ffe9fe24f1ee5a4d94ab7dbba171
Abhishek <appalashettyabhishek23@gmail.com>
2023-11-29 23:03:57 -0600

    made changes

--- models/eagle.js
+++ models/eagle.js
@@ -1,8 +1,18 @@
-const mongoose = require("mongoose")
-const eagleSchema = mongoose.Schema({
-    name: String,
-    age: Number,
-    description: String
-})
-module.exports = mongoose.model("eagle",
-    eagleSchema)
+  const mongoose = require("mongoose");
+
+  const eagleSchema = mongoose.Schema({
+      name: String,
+      age: {
+          type: Number,
+          max: 20, // Set your maximum allowed value
+          validate: {
+              validator: function(value) {
+                  return value <= 20;
+              },
+              message: 'Age cannot exceed 20',
+          },
+      },
+      description: String,
+  });
+  
+  module.exports = mongoose.model("eagle", eagleSchema);
--- routes/eagles.js
+++ routes/eagles.js
@@ -1,24 +1,41 @@
 var express = require("express");
 const eagle_controlers = require("../controllers/eagleController");
+const passport = require('passport');
+
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
 // GET eagles
 router.get("/", eagle_controlers.eagle_view_all_Page);
-module.exports = router;
 
 // GET request for one eagle.
 router.get('/eagles/:id', eagle_controlers.eagle_detail);
 
-// PUT request to update a eagle.
+// PUT request to update an eagle.
 router.put('/eagles/:id', eagle_controlers.eagle_update_put);
 
 /* GET detail eagle page */
 router.get('/detail', eagle_controlers.eagle_view_one_Page);
 
 /* GET create eagle page */
-router.get('/create', eagle_controlers.eagle_create_Page);
-
-/* GET create eagle page */
-router.get('/update', eagle_controlers.eagle_update_Page);
+router.get('/create', secured, eagle_controlers.eagle_create_Page);
 
 /* GET delete eagle page */
-router.get('/delete', eagle_controlers.eagle_delete_Page);
+router.get('/delete', secured, eagle_controlers.eagle_delete_Page);
+
+/* GET update eagle page */
+router.get('/update', secured, eagle_controlers.eagle_update_Page);
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
+
+module.exports = router;
